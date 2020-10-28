f3d63e26d7a65cc411e56cddb068546d7e523881
mell <mell.mmm.1022@gmail.com>
2020-10-28 23:14:45 +0900

    routes用のフォルダとファイルをダミーで作成

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const cors = require('cors');
+
+
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.disable('x-powered-by');
 
+// APIのバージョン
+const apiVer = '/api/v1';
+
+// パス
+app.use(`/${apiVer}/auth`, require('./src/routes/auth'));
+
+
+// エラー処理
+// TODO: 返却するステータスコードをエラーメッセージは可変にする
 app.use((req, res) => res.status(404).json({ message: 'Not found' }));
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: 'Server Error' });
 })
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.info(`server has started on ${PORT}`))
---
+++ src/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('', (req, res) => {
+    if(req.query.test){
+        res.send(`get request ${req.query.test}`)
+    } else {
+        res.send(`get request`)
+    }
+});
+
+
+module.exports = router;
