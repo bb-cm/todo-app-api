fca7ad61f7d84af19b1becc759611486865db7bd
mell <mell.mmm.1022@gmail.com>
2020-11-04 21:32:51 +0900

    prettierのフォーマットとマイグレーションファイル正常に動いていなかったので修正

--- src/db/migrations/20201030003015_migration_create_table.js
+++ src/db/migrations/20201030003015_migration_create_table.js
@@ -1,35 +1,41 @@
-exports.up = (knex) => knex.schema
-  .createTable('user', (table) => {
-    table.increments().primary();
-    table.varchar('email', 128).notNullable().unique();
-    table.varchar('password', 128).notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
-  .createTable('todo', (table) => {
-    table.increments().primary();
-    table.bigint('user_id').notNullable();
-    table
-      .foreign('user_id')
-      .references('id')
-      .inTable('user')
-      .onDelete('CASCADE');
-    table.varchar('title', 100).notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
-  .createTable('todo_detail', (table) => {
-    table.increments().primary();
-    table.bigint('todo_id').notNullable();
-    table
-      .foreign('todo_id')
-      .references('id')
-      .inTable('todo')
-      .onDelete('CASCADE');
-    table.varchar('task', 100).notNullable();
-    table.boolean('is_done').notNullable().defaultTo(false);
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  });
+exports.up = (knex) => {
+  const sql = knex.schema
+    .createTable('user', (table) => {
+      table.increments().primary();
+      table.varchar('email', 128).notNullable().unique();
+      table.varchar('password', 128).notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('todo', (table) => {
+      table.increments().primary();
+      table.bigint('user_id').notNullable();
+      table
+        .foreign('user_id')
+        .references('id')
+        .inTable('user')
+        .onDelete('CASCADE');
+      table.varchar('title', 100).notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('todo_detail', (table) => {
+      table.increments().primary();
+      table.bigint('todo_id').notNullable();
+      table
+        .foreign('todo_id')
+        .references('id')
+        .inTable('todo')
+        .onDelete('CASCADE');
+      table.varchar('task', 100).notNullable();
+      table.boolean('is_done').notNullable().defaultTo(false);
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+  return sql;
+};
 
-exports.down = (knex) => knex.schema.dropTable('posts').dropTable('users').dropTable('comments');
+exports.down = (knex) => {
+  const sql = knex.schema.dropTable('posts').dropTable('users').dropTable('comments');
+  return sql;
+};
--- src/routes/sample.js
+++ src/routes/sample.js
@@ -4,15 +4,9 @@ const router = express.Router();
 const sampleController = require('../controllers/sample');
 
 // GETサンプル
-router.get(
-  '',
-  sampleController.getSomething,
-);
+router.get('', sampleController.getSomething);
 
 // GETサンプル パス指定
-router.get(
-  '/path',
-  sampleController.getPath,
-);
+router.get('/path', sampleController.getPath);
 
 module.exports = router;
