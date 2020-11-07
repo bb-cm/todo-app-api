c810e391919710b092e42d84c854a270d7731c1f
chanbun <shin0109biwa.com@gmail.com>
2020-11-07 20:23:45 +0900

    マイグレーションとシーダーファイル内テーブル名の修正

--- src/db/seeds/seed_create_todos.js
+++ src/db/seeds/seed_create_todos.js
@@ -1,8 +1,8 @@
 exports.seed = async (knex) => {
-  await knex('todo').del();
+  await knex('todos').del();
 
-  const userInfo = await knex('user');
-  await knex('todo').insert([
+  const userInfo = await knex('users');
+  await knex('todos').insert([
     { user_id: userInfo[0].id, title: '親_todo1' },
     { user_id: userInfo[0].id, title: '親_todo2' },
     { user_id: userInfo[0].id, title: '親_todo3' },
