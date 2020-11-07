04c51e98c7fe56731ebc04afbce32bce5ebe029b
mell <mell.mmm.1022@gmail.com>
2020-11-07 21:32:05 +0900

    Todo取得API修正  スキーマ追加

--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
+const validator = require('express-joi-validation').createValidator({
+  passError: true,
+});
+const todoSchema = require('../schemas/todo');
+
 const todoController = require('../controllers/todo');
 
 // Todo取得
-router.get('', todoController.getTodo);
+router.get('', validator.query(todoSchema.getTodo), todoController.getTodo);
 
 module.exports = router;
